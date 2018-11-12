d445138601df2d4b75835178892247a4de2258d4
Unknown <zhangbolily@gmail.com>
2018-11-12 23:09:19 +0800

    Add: An example

---
+++ Examples/MultiThreadsHelloWorld/src/main.cpp
@@ -0,0 +1,14 @@
+#include "BAbstractTask.h"
+#include <iostream>
+
+using namespace std;
+using namespace BThreadPack;
+
+int main(int argc, char** argv)
+{
+    BAbstractTask _temp_task;
+
+    cout<<"This will be a multi-thread hello world!"<<endl;
+
+    return 0;
+}
