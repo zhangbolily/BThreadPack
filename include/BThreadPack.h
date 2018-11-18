fc0119772ec0c393496588ecc38f9ee04b2c6e71
Unknown <zhangbolily@gmail.com>
2018-11-18 15:10:41 +0800

    Change: Changed the output error message text color
    Changed to red backgroud color with white text

--- include/BThreadPack.h
+++ include/BThreadPack.h
@@ -67,7 +67,10 @@
 #define B_PRINT_ERROR(msg)  \
 {\
     std::ostringstream _os;\
-    _os << "[Error] " << msg << "\n";\
+    std::time_t result = std::time(nullptr);\
+    char time_str[64] = {0};\
+    std::strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S ", std::localtime(&result));\
+    _os << "\033[37;41m[Error] Thread "<< this_thread::get_id() << " " << time_str << msg << "\033[0m\n";\
     std::cout << _os.str();\
 }
 #endif
