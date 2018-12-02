3162a8d4e2ea2938905c86e4e6c8009b72b93772
Unknown <zhangbolily@gmail.com>
2018-12-02 23:11:18 +0800

    Add: Task management

--- src/BAbstractThreadPool/BAbstractThreadPool.cpp
+++ src/BAbstractThreadPool/BAbstractThreadPool.cpp
@@ -14,6 +14,7 @@ namespace BThreadPack{
 BAbstractThreadPool::BAbstractThreadPool(int _threadNum)
     :_threadNum_(_threadNum)
 {
+    this->_initThreads_();
 }
 
 /* @~BAbstractThreadPool() - Destructor
@@ -49,10 +50,22 @@ int BAbstractThreadPool::startAllTask()
 }
 
 /* @kill - Kill all threads and release all resources.
+ * Actually this function do nothing now.
  * Don't need any parameters.
 */
 int BAbstractThreadPool::kill()
 {
+    return 0;
+}
+
+/* @addTask - Add a task to the task queue.
+ * @_taskBuffer - A memory buffer that contains the task data.
+ * @return - return 0 if success
+*/
+int BAbstractThreadPool::addTask(void* _taskBuffer)
+{
+    this->_taskQueue_.push(_taskBuffer);
+    
     return 0;
 }
 
@@ -61,14 +74,17 @@ int BAbstractThreadPool::kill()
 */
 int BAbstractThreadPool::_initThreads_()
 {
-    for (int i = 0; i < this->_threadNum_; ++i) {
-        this->_threadVec_.push_back(std::thread(_threadFunction_, NULL));
+    //TODO: Define a temp pointer.
+    void* _tempPointer = nullptr;
+    
+    for (unsigned int i = 0; i < this->_threadNum_; ++i) {
+        this->_threadVec_.push_back(thread(BAbstractThreadPool::_threadFunction_, ref(_tempPointer)));
     }
     
     return 0;
 }
 
-void BAbstractThreadPool::_threadFunction_(void * _buffer)
+void BAbstractThreadPool::_threadFunction_(void* _buffer)
 {
     //Do nothing.
 }
